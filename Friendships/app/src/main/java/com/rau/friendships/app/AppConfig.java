1fa704fe705786b51920f89a9f4737f99fc13db5
ubo32 <ubojorquez@ucdavis.edu>
2015-05-31 10:58:09 -0700

    added: volley files, synced with gradle todo: finish login

---
+++ Friendships/app/src/main/java/com/rau/friendships/app/AppConfig.java
@@ -0,0 +1,6 @@
+package com.rau.friendships.app;
+
+public class AppConfig {
+    public static String URL_LOGIN = "http://198.27.67.84/share/android_login_api/";
+    public static String URL_REGISTER = "http://198.27.67.84/share/android_login_api/";
+}
---
+++ Friendships/app/src/main/java/com/rau/friendships/helper/SQLiteHandler.java
@@ -0,0 +1,116 @@
+package com.rau.friendships.helper;
+
+import java.util.HashMap;
+import android.util.Log;
+import android.content.Context;
+import android.database.Cursor;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class SQLiteHandler extends SQLiteOpenHelper {
+
+    private static final String TAG = SQLiteHandler.class.getSimpleName();
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "android";
+
+    // Login table name
+    private static final String TABLE_LOGIN = "login";
+
+    // Login Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_UID = "uid";
+    private static final String KEY_CREATED_AT = "created_at";
+
+    public SQLiteHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_LOGIN + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_EMAIL + " TEXT UNIQUE," + KEY_UID + " TEXT,"
+                + KEY_CREATED_AT + " TEXT" + ")";
+        db.execSQL(CREATE_LOGIN_TABLE);
+
+        Log.d(TAG, "Database tables created");
+    }
+
+    // upgrading db
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOGIN);
+        onCreate(db);
+    }
+
+    // store details in db
+    public void addUser(String name, String email, String uid, String created_at) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_EMAIL, email);
+        values.put(KEY_UID, uid);
+        values.put(KEY_CREATED_AT, created_at);
+
+        long id = db.insert(TABLE_LOGIN, null, values);
+        db.close();
+
+        Log.d(TAG, "New user inserted into SQlite: " + id);
+    }
+
+    // retrieve details from db
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT  * FROM " + TABLE_LOGIN;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            user.put("name", cursor.getString(1));
+            user.put("email", cursor.getString(2));
+            user.put("uid", cursor.getString(3));
+            user.put("created_at", cursor.getString(4));
+        }
+        cursor.close();
+        db.close();
+
+        Log.d(TAG, "Fetching user from SQlite: " + user.toString());
+
+        return user;
+    }
+
+    // login status
+    public int getRowCount() {
+        String countQuery = "SELECT  * FROM " + TABLE_LOGIN;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        int rowCount = cursor.getCount();
+        db.close();
+        cursor.close();
+
+        return rowCount;
+    }
+
+    // delete user and update db
+    public void deleteUsers() {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(TABLE_LOGIN, null, null);
+        db.close();
+
+        Log.d(TAG, "Deleted all user info from SQlite");
+    }
+
+}
